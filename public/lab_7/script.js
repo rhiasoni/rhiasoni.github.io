78f6d45924de3fd46e796054441e67e34aa16d7b
Rhia Soni <rhiasoni@gmail.com>
2020-10-30 21:03:55 -0400

    final

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -1,19 +1,91 @@
 function convertRestaurantsToCategories(restaurantList) {
   // process your restaurants here!
-  return list;
+  function range(int) {
+    const arr = [];
+    for (let i = 0; i < int; i += 1) {
+      arr.push(i);
+    }
+    return arr;
+  }
+  
+  function getRandomIntInclusive(min, max) {
+    const min1 = Math.ceil(min);
+    const max1 = Math.floor(max);
+    return Math.floor(Math.random() * (max1 - min1 + 1) + min1); // The maximum is inclusive and the minimum is inclusive
+  }
+  
+  async function loadData() {
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+  
+    const arrayOfTenItems = range(40);
+    const randomRestaurantsArray = arrayOfTenItems.map((item) => {
+      const which = getRandomIntInclusive(0, json.length);
+      const restaurant = json[which]; // we are not worrying about uniqueness here
+      return restaurant;
+    });
+  
+    console.table(randomRestaurantsArray); // This shows the shape of our data as it arrives
+  
+    const div = document.createElement('div');
+    div.innerHTML = `<h2>What we have</h2> <br />${JSON.stringify(randomRestaurantsArray[0])}<br /><br />`;
+    $('body').append(div);
+  
+    /// And now, how to get what we want
+    const newDataShape = randomRestaurantsArray.reduce((collection, item, i) => {
+      // for each item, check if we have a category for that item already
+      const findCat = collection.find((findItem) => findItem.label === item.category);
+      
+      if (!findCat) {
+        collection.push({
+          label: item.category,
+          y: 1
+        });
+      } else {
+        const position = collection.findIndex(el => el.label === item.category);
+        collection[position].y += 1;
+      }
+      return collection;
+    }, []);
+  
+    console.table(newDataShape);
+  
+    const div2 = document.createElement('div');
+    const obj = {
+      label: randomRestaurantsArray[0].category,
+      y: randomRestaurantsArray.length
+    };
+    div2.innerHTML = `<h2>What we want</h2> <br /> <h4>A category, how many things are in the category</h4><pre><code class="language-javascript">${JSON.stringify(obj)}</pre></code>`;
+  
+    $('body').append(div2);
+  }
+  
+  window.onload = loadData;
+  //return list;
 }
 
 function makeYourOptionsObject(datapointsFromRestaurantsList) {
   // set your chart configuration here!
   CanvasJS.addColorSet('customColorSet1', [
     // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
+    //colorSet Array
+    '#4661EE',
+    '#EC5657',
+    '#1BCDD1',
+    '#8FAABB',
+    '#B08BEB',
+    '#3EA0DD',
+    '#F5A52A',
+    '#23BFAA',
+    '#FAA586',
+    '#EB8CC6',
   ]);
 
   return {
     animationEnabled: true,
     colorSet: 'customColorSet1',
     title: {
-      text: 'Change This Title'
+      text: 'Places To Eat Out In Future'
     },
     axisX: {
       interval: 1,
@@ -22,9 +94,26 @@ function makeYourOptionsObject(datapointsFromRestaurantsList) {
     axisY2: {
       interlacedColor: 'rgba(1,77,101,.2)',
       gridColor: 'rgba(1,77,101,.1)',
-      title: 'Change This Title',
+      title: 'Restaurants By Category',
       labelFontSize: 12,
-      scaleBreaks: {customBreaks: []} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
+      scaleBreaks: {customBreaks: [{
+        startValue: 40,
+        endValue: 50,
+        color: "orange",
+        type: "zigzag"
+      },
+      {
+        startValue: 90,
+        endValue: 100,
+        color: "yellow",
+        type: "zigzag"
+      },
+      {
+        startValue: 140,
+        endValue: 150,
+        color: "green",
+        type: "waved"
+      }]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
     },
     data: [{
       type: 'bar',
